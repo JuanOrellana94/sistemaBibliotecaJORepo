aac49e4ade85d28651a7f9fd64e5b06f9d99f12a
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-06-27 21:00:08 -0600

    fix editoriales/editorales
    Arreglar el nombre de una variable

--- catalogos.php
+++ catalogos.php
@@ -16,7 +16,7 @@ if ($pageLocation=="libros") {
     include("pages/catalogs/libros.php");
  } else if ($pageLocation=="autores") {
  	include("pages/autores/verAutores.php");
- }else if ($pageLocation=="editorales") {
+ }else if ($pageLocation=="editoriales") {
  	include("pages/editoriales/verEditoriales.php");
  }else if ($pageLocation=="estantes") {
  	include("pages/estantes/verEstantes.php");
